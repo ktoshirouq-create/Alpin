476956515a7759892b074bddce219ec7ded60bfc
ktoshirouq-create <ktoshirouq@gmail.com>
2026-09-21 17:31:28 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Offline support. The page itself is always fetched fresh when online,
 // so edits to index.html show up without touching this file.
-const CACHE = 'training-guide-1';
+const CACHE = 'training-guide-2';
 const CORE = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
